14dd721b5eb97966852561856c92d5e9c51e8bbf
wongsrila <jiraphong.wongsrila@gmail.com>
2023-05-25 15:28:40 +0200

    werkt?

--- main.js
+++ main.js
@@ -61,74 +61,11 @@ var voice = {
 };
 window.addEventListener('DOMContentLoaded', voice.init);
 
-// import 'cropperjs/dist/cropper.css';
 import Cropper from 'cropperjs';
 
-// const image = document.getElementById('image');
-// const rotateBtn = document.querySelector('.rotate-btn');
-// // const cropBtn = document.querySelector('.crop-btn');
 const result = document.getElementById('result');
 
 let cmd = {};
-
-// const cropper = new Cropper(image, {
-// viewMode: 1,
-// center: true,
-// autoCrop: false,
-// crop(event) {
-//   result.innerHTML = '';
-//   result.appendChild(cropper.getCroppedCanvas());
-// },
-//   ready() {
-//     cmd = {
-//       'zoom in': () => {
-//         this.cropper.zoom(0.1);
-//       },
-
-//       'zoom out': () => {
-//         this.cropper.zoom(-0.1);
-//       },
-
-//       rotate: () => {
-//         this.cropper.rotate(45);
-//       },
-
-//       'rotate twice': () => {
-//         this.cropper.rotate(90);
-//       },
-
-//       'move right': () => {
-//         this.cropper.move(-20, 0);
-//       },
-//       'move left': () => {
-//         this.cropper.move(20, 0);
-//       },
-
-//       'flip horizontal': () => {
-//         this.cropper.scale(-1, 1);
-//       },
-//       'flip vertical': () => {
-//         this.cropper.scale(1, -1);
-//       },
-//       'flip reset': () => {
-//         this.cropper.scale(1);
-//       },
-
-//       reset: () => {
-//         this.cropper.reset();
-//       },
-
-//       'start editing': () => {
-//         this.cropper.crop();
-//       },
-//     };
-//     // And then
-//     this.cropper.crop();
-//     cropBtn.addEventListener('click', () => {
-//       this.cropper.getCroppedCanvas({ maxWidth: 4096, maxHeight: 4096 });
-//     });
-//   },
-// });
 
 window.addEventListener('DOMContentLoaded', function () {
   var avatar = document.getElementById('avatar');
@@ -231,65 +168,20 @@ window.addEventListener('DOMContentLoaded', function () {
     });
 
   document.getElementById('crop').addEventListener('click', function () {
-    var initialAvatarURL;
-    var canvas;
-
-    $modal.modal('hide');
-
     if (cropper) {
-      canvas = cropper.getCroppedCanvas({
-        width: 160,
-        height: 160,
-      });
-      initialAvatarURL = avatar.src;
-      avatar.src = canvas.toDataURL();
-      $progress.show();
-      $alert.removeClass('alert-success alert-warning');
-      canvas.toBlob(function (blob) {
-        var formData = new FormData();
+      let canvas = cropper.getCroppedCanvas();
+      // Convert our canvas to a data URL
+      let canvasUrl = canvas.toDataURL();
+      // Create an anchor, and set the href value to our data URL
+      const createEl = document.createElement('a');
+      createEl.href = canvasUrl;
 
-        formData.append('avatar', blob, 'avatar.jpg');
-        console.log(formData);
-        $.ajax('https://jsonplaceholder.typicode.com/posts', {
-          method: 'POST',
-          data: formData,
-          processData: false,
-          contentType: false,
+      // This is the name of our downloaded file
+      createEl.download = 'cropped-image';
 
-          xhr: function () {
-            var xhr = new XMLHttpRequest();
-
-            xhr.upload.onprogress = function (e) {
-              var percent = '0';
-              var percentage = '0%';
-
-              if (e.lengthComputable) {
-                percent = Math.round((e.loaded / e.total) * 100);
-                percentage = percent + '%';
-                $progressBar
-                  .width(percentage)
-                  .attr('aria-valuenow', percent)
-                  .text(percentage);
-              }
-            };
-
-            return xhr;
-          },
-
-          success: function () {
-            $alert.show().addClass('alert-success').text('Upload success');
-          },
-
-          error: function () {
-            avatar.src = initialAvatarURL;
-            $alert.show().addClass('alert-warning').text('Upload error');
-          },
-
-          complete: function () {
-            $progress.hide();
-          },
-        });
-      });
+      // Click the download button, causing a download, and then remove it
+      createEl.click();
+      createEl.remove();
     }
   });
 });
